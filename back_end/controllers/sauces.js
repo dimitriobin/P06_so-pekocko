f43f5380bd165854a012f4238bed0f38b1c72983
Dimitri Obin <dimitriobin@gmail.com>
2020-10-02 19:59:35 +0200

    création du model pour la sauce et intégration des middlewares pour les opérations CRUD

--- back_end/controllers/sauces.js
+++ back_end/controllers/sauces.js
@@ -1,19 +1,75 @@
-exports.getAllSauces = (req, res, next) => {
-    res.send('GET ALL SAUCES !');
+const Sauce = require('../models/Sauce');
+
+exports.getAllSauces = async (req, res, next) => {
+    try {
+        const allSauces = await Sauce.find();
+        res.status(200).json(allSauces);
+    } catch (error) {
+        res.status(400).json({
+            error
+        });
+    }
 };
 
-exports.getOneSauce = (req, res, next) => {
-    res.send('GET ONE SAUCE!');
+exports.getOneSauce = async (req, res, next) => {
+    try {
+        const sauce = await Sauce.findOne({
+            _id: req.params.id
+        });
+        res.status(200).json(sauce);
+    } catch (error) {
+        res.status(404).json({
+            error
+        });
+    }
 };
 
-exports.createOneSauce = (req, res, next) => {
-    res.send('CREATE ONE SAUCE !');
+exports.createOneSauce = async (req, res, next) => {
+    try {
+        delete req.body._id;
+        const sauce = new Sauce({
+            ...req.body
+        });
+        await sauce.save()
+        res.status(201).json({
+            message: 'Vous avez ajouté une nouvelle sauce !'
+        });
+    } catch (error) {
+        res.status(400).json({
+            error
+        });
+    }
 };
 
-exports.updateOneSauce = (req, res, next) => {
-    res.send('UPDATE ONE SAUCE !');
+exports.updateOneSauce = async (req, res, next) => {
+    try {
+        await Sauce.updateOne({
+            _id: req.params.id
+        }, {
+            ...req.body,
+            _id: req.params.id
+        });
+        res.status(200).json({
+            message: 'Votre sauce a été modifiée !'
+        });
+    } catch (error) {
+        res.status(400).json({
+            error
+        });
+    }
 };
 
-exports.deleteOneSauce = (req, res, next) => {
-    res.send('DELETE ONE SAUCE !');
+exports.deleteOneSauce = async (req, res, next) => {
+    try {
+        await Sauce.deleteOne({
+            _id: req.params.id
+        })
+        res.status(200).json({
+            message: 'Cette sauce a été supprimée !'
+        });
+    } catch (error) {
+        res.status(400).json({
+            error
+        });
+    }
 };
