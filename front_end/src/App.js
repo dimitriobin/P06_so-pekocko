7974f873986e3cc54f571b0c431f0a69d68fdce4
Dimitri Obin <dimitriobin@gmail.com>
2021-03-19 16:19:48 +0100

    create the navigation header

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -10,23 +10,15 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <img className="mx-auto my-5" src={logo} alt="logo de l'application"></img>
-        <ul>
-          <li>
-            <Link to={"/sauces"}>All sauces</Link>
-          </li>
-          <li>
-            <Link to={"/sauce/1"}>One sauce</Link>
-          </li>
-          <li>
-            <Link to={"/add"}>Add sauce</Link>
-          </li>
-        </ul>
+        <Link to={"/sauces"} >  
+          <img src={logo} alt="logo de l'application" className="inline my-10"></img>
+        </Link>
+        <button className="absolute top-5 md:top-10 lg:top-20 right-5 md:right-10 lg:right-20" >
+          <i class="fas fa-sign-out-alt fa-3x"></i>
+        </button>
       </nav>
       <Switch>
         <Route exact path={['/', '/sauces']} component={SauceList} />
-        <Route exact path='/add' component={AddSauce} />
-        <Route path='/sauce/:id' component={Sauce} />
       </Switch>
     </div>
   );
