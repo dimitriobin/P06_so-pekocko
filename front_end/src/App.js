b07306457af1c4405526371bf50ed3d8c9669630
Dimitri Obin <dimitriobin@gmail.com>
2021-03-21 19:06:40 +0100

    create the register logic

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,34 +1,69 @@
 import logo from "./logo.svg";
 import { Switch, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 import SauceList from "./components/SaucesList";
 import Sauce from "./components/Sauce";
 import AddSauce from "./components/AddSauce";
+import Login from "./components/Login";
+import Signup from "./components/Register";
+import AuthService from "./services/AuthServices";
 
 function App() {
   const addSauce = false;
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
+
   return (
     <div className="App min-h-screen flex flex-col justify-between items-center relative">
-      <nav className="w-full flex items-center justify-center mb-10 lg:mb-20">
-        <Link to={"/sauces"}>
+      <nav className="container relative flex items-center justify-center mb-10 lg:mb-20">
+        <Link to={"/"}>
           <img
             src={logo}
             alt="logo de l'application"
             className="inline my-10"
           ></img>
         </Link>
-        <button className="absolute top-5 md:top-10 lg:top-20 right-5 md:right-10 lg:right-20">
-          <i className="fas fa-sign-out-alt fa-3x"></i>
-        </button>
+        {currentUser ? (
+          <button className="absolute top-5 md:top-10 lg:top-20 right-5 md:right-10 lg:right-20">
+            <i className="fas fa-sign-out-alt fa-3x"></i>
+          </button>
+        ) : (
+          <div className="absolute right-0">
+            <Link to={"/signup"} className="mr-5">
+              Signup
+            </Link>
+            <Link to={"/login"}>Login</Link>
+          </div>
+        )}
       </nav>
-      <Switch>
-        <div className="container mx-auto">
-          <Route exact path={["/", "/sauces"]} component={SauceList} />
-          <Route path={"/sauce/:id"} component={Sauce} />
-        </div>
-      </Switch>
-      {addSauce && <AddSauce />}
+
+      {currentUser ? (
+        <>
+          <Switch>
+            <div className="container mx-auto">
+              <Route exact path={["/", "/sauces"]} component={SauceList} />
+              <Route path={"/sauce/:id"} component={Sauce} />
+            </div>
+          </Switch>
+          {addSauce && <AddSauce />}
+        </>
+      ) : (
+        <Switch>
+          <div className="container mx-auto">
+            <Route exact path={["/", "/login"]} component={Login} />
+            <Route path={"/signup"} component={Signup} />
+          </div>
+        </Switch>
+      )}
       <footer className="bg-gray-900 text-white w-full p-10 flex flex-wrap justify-evenly items-center mt-20">
         <p className="my-1">Copyright 2021</p>
         <a className="underline my-1" href="dimitriobin.com">
