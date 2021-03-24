a381450d93bfd7ec7d84613127f2116dd3aa5916
Dimitri Obin <dimitriobin@gmail.com>
2021-03-24 16:41:14 +0100

    make validations

--- front_end/src/components/AddSauce.jsx
+++ front_end/src/components/AddSauce.jsx
@@ -1,59 +1,136 @@
-function AddSauce({ showSauceForm }) {
-  const image_url = false;
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { useRef, useState } from "react";
+import { isLength } from "validator";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="font-medium text-red-500" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+const length = (value) => {
+  if (!isLength(value, { min: 5, max: 200 })) {
+    return (
+      <div className="font-medium text-red-500" role="alert">
+        This must be between 5 and 200 characters
+      </div>
+    );
+  }
+};
+
+function AddSauce(props) {
+  const initialSauce = {
+    userId: "",
+    name: "",
+    manufacturer: "",
+    description: "",
+    mainPepper: "",
+    imageUrl: false,
+    heat: 0,
+  };
+  const [sauce, setSauce] = useState(initialSauce);
+  const [preview, setPreview] = useState(null);
+  const form = useRef();
+  const checkBtn = useRef();
+  const imageInput = useRef();
 
   const handleCloseSauceForm = (e) => {
     e.preventDefault();
+    props.showSauceForm(false);
+  };
 
-    showSauceForm(false);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    form.current.validateAll();
+    if (checkBtn.current.context._errors === 0) {
+      console.log(checkBtn.current.context._errors.length);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === "imageUrl") {
+      setSauce({
+        ...sauce,
+        imageUrl: e.target.files[0],
+      });
+      setPreview(URL.createObjectURL(e.target.files[0]));
+    } else {
+      setSauce({
+        ...sauce,
+        [e.target.name]: e.target.value,
+      });
+    }
   };
 
   return (
     <div className="popup__bg bg-black bg-opacity-80 absolute top-0 right-0 left-0 bottom-0 z-10">
-      <form className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 md:p-8 lg:p-10 w-11/12 md:w-8/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 rounded-3xl flex flex-col">
+      <Form
+        onSubmit={handleFormSubmit}
+        ref={form}
+        className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 md:p-8 lg:p-10 w-11/12 md:w-8/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 rounded-3xl flex flex-col"
+      >
         <label htmlFor="name" className="font-medium mb-1">
           Sauce's name
         </label>
-        <input
+        <Input
           type="text"
           name="name"
           id="name"
-          className="border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
+          value={sauce.name}
+          onChange={handleChange}
+          validations={[required, length]}
+          className="w-full border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
         />
-        <label htmlFor="creator" className="font-medium mb-1">
-          Sauce's creator
+        <label htmlFor="manufacturer" className="font-medium mb-1">
+          Sauce's manufacturer
         </label>
-        <input
+        <Input
           type="text"
-          name="creator"
-          id="creator"
-          className="border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
+          name="manufacturer"
+          id="manufacturer"
+          value={sauce.manufacturer}
+          onChange={handleChange}
+          validations={[required, length]}
+          className="w-full border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
         />
         <label htmlFor="description" className="font-medium mb-1">
           Description
         </label>
-        <input
+        <Input
           type="text"
           name="description"
           id="description"
-          className="border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
+          value={sauce.description}
+          onChange={handleChange}
+          validations={[required, length]}
+          className="w-full border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
         />
-        <label htmlFor="ingredient" className="font-medium mb-1">
+        <label htmlFor="mainPepper" className="font-medium mb-1">
           Main ingredient
         </label>
-        <input
+        <Input
           type="text"
-          name="ingredient"
-          id="ingredient"
-          className="border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
+          name="mainPepper"
+          id="mainPepper"
+          value={sauce.mainPepper}
+          onChange={handleChange}
+          validations={[required, length]}
+          className="w-full border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
         />
-        {image_url ? (
+        {sauce.imageUrl ? (
           <label
             htmlFor="image"
             className="w-28 h-28 rounded-full font-medium cursor-pointer mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white focus:outline-none transition-all"
             tabIndex="0"
           >
             <img
-              src="https://picsum.photos/800"
+              src={preview}
               alt="Preview of you sauce"
               className="flex w-28 h-28 rounded-full mb-1 cursor-pointer flex-col justify-center items-center border-2 border-black hover:opacity-90 focus:outline-none transition-all"
             />
@@ -64,26 +141,33 @@ function AddSauce({ showSauceForm }) {
             className="w-28 h-28 rounded-full font-medium cursor-pointer mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white focus:outline-none transition-all"
             tabIndex="0"
           >
-            <span
-              src="https://picsum.photos/800"
-              alt="Preview of you sauce"
-              className="flex text-center w-28 h-28 rounded-full mb-1 cursor-pointer flex-col justify-center items-center border-2 border-black hover:opacity-90 focus:outline-none transition-all"
-            >
+            <span className="flex text-center w-28 h-28 rounded-full mb-1 cursor-pointer flex-col justify-center items-center border-2 border-black hover:opacity-90 focus:outline-none transition-all">
               Add an image
             </span>
           </label>
         )}
-        <input type="file" name="image" id="image" className="hidden" />
+        <Input
+          type="file"
+          name="imageUrl"
+          id="image"
+          className="hidden"
+          ref={imageInput}
+          validations={[required]}
+          onChange={handleChange}
+        />
         <label htmlFor="rank" className="font-medium mb-1">
           Hot rank
         </label>
-        <input
+        <Input
           type="range"
-          name="rank"
+          name="heat"
           id="rank"
           min="1"
           max="5"
-          className="border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
+          value={sauce.heat}
+          onChange={handleChange}
+          validations={[required]}
+          className="w-full border-2 border-black rounded px-3 py-2 text-xl mb-4 focus:ring focus:ring-yellow-600 focus:ring-offset-4 focus:ring-offset-white  transition-all"
         />
         <div className="flex justify-evenly">
           <button
@@ -99,7 +183,8 @@ function AddSauce({ showSauceForm }) {
             Back
           </button>
         </div>
-      </form>
+        <CheckButton style={{ display: "none" }} ref={checkBtn} />
+      </Form>
     </div>
   );
 }
