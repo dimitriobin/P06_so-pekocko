8e544f8a3c177842531f9618cee36e09838a467c
Dimitri Obin <dimitriobin@gmail.com>
2023-11-14 22:29:21 +0100

    Update docker containers ports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 const app = require("./server");
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import sauceRouter from "./routes/sauces";
 import userRouter from "./routes/users";
+import cors from "cors";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/sauces", sauceRouter);
 app.use("/api/auth", userRouter);
